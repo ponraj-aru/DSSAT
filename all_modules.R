2a0c560ef3ecdff2dfe4052cd117b2a4714b252f
ponraj <ponraj.aru@gmail.com>
2025-04-29 17:03:19 +0200

    adding line 1 just to test

--- all_modules.R
+++ all_modules.R
@@ -1,19 +1,21 @@
+# I am testing the push and pull
+
 # Every time need to run
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Treatment.R")
 # Ev
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Cultivar.R")
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Cultivar.R")
 #source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/FILEX.R")
-#no need 
+#no need
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Fields.R")
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Fields.R")
 # Need to when you change plantind date (one time run )
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Initial_Conditions.R")
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Initial_Conditions.R")
-# Need to when you change irrigation 
-#file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Planting_Details.R") 
-source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Planting_Details.R") 
-source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Harvesting_Details.R") 
+# Need to when you change irrigation
+#file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Planting_Details.R")
+source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Planting_Details.R")
+source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Harvesting_Details.R")
 
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/fertlizer.R")
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/residue.R")
@@ -23,11 +25,11 @@ file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Irrigation.R")
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Automatic_Management.R")
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Automatic_Management.R")
-# Run it 
+# Run it
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Simulation_controls.R")
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Simulation_controls.R")
-# RUn it 
-file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/join_ama_sim.R")# Run it 
+# RUn it
+file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/join_ama_sim.R")# Run it
 
 # No need
 #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/FILEX.R")
@@ -38,8 +40,8 @@ source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK
 source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/FILEX.R")
 
 
-# 
-# 
+#
+#
 # source("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Treatment.R")
 # # Ev
 # #file.edit("C:/Users/arumu002/OneDrive - Wageningen University & Research/ponraj_PIK_data/filex/Codes/Cultivar.R")
